ba7fe1a45c0626c15d35a1d0ee49741b4e4dcb7c
PiGodzyh <237793303@qq.com>
2024-10-08 21:32:43 +0800

    user.js的注释添加

--- server/api/user.js
+++ server/api/user.js
@@ -4,6 +4,19 @@
 // import Message from "@/server/Message.js"  // 在每个 api 文件里都要引入这两个文件
 
 const userService = {
+/**
+ * 接口名称：用户登录接口。
+ * 接口定义：此接口用于用户登录。
+ * 输入：
+ *   - body：
+ *      - username
+ *      - password
+ * 输出：
+ *   - 在获取成功的情况下：
+ *      - 用户的认证信息 authentication。
+ *   - 在获取失败的情况下：
+ *      - 无输出。有错误信息显示。
+ */
     async login(email, password) {
         try {
             console.log("login")
@@ -22,8 +35,19 @@ const userService = {
             Message.error(error.message)
         }
     },
-
-    async sendVerificationCode (email) {
+/**
+ * 接口名称：发送验证码接口。
+ * 接口定义：此接口用于给指定邮箱发送验证码。
+ * 输入：
+ *   - body：
+ *      - email
+ * 输出：
+ *   - 在获取成功的情况下：
+ *      - true 值。同时会有成功信息显示。
+ *   - 在获取失败的情况下：
+ *      - false 值。同时会有错误信息显示。
+ */
+    async sendVerificationCode(email) {
         try {
             const response = await Request.post('/api/account/sendCode', { "email": email });
             if (response.status === 200) {
@@ -38,8 +62,21 @@ const userService = {
             return false;
         }
     },
-
-    async register (email, code, password) {
+/**
+ * 接口名称：用户注册接口。
+ * 接口定义：此接口用于用户注册。
+ * 输入：
+ *   - body：
+ *      - email
+ *      - code 验证码
+ *      - password
+ * 输出：
+ *   - 在获取成功的情况下：
+ *      - true 值。同时会有成功信息显示。
+ *   - 在获取失败的情况下：
+ *      - false 值。同时会有错误信息显示。
+ */
+    async register(email, code, password) {
         try {
             const response = await Request.post('/api/account/validate', { "email": email, "code": code, "password": password });
             if (response.status === 200) {
@@ -54,8 +91,19 @@ const userService = {
             return false;
         }
     },
-
-    async getCurrentInfo (user) {
+/**
+ * 接口名称：用户信息接口。
+ * 接口定义：此接口用于获取用户信息。
+ * 输入：
+ *   - headers：
+ *      - Authorization 认证信息
+ * 输出：
+ *   - 在获取成功的情况下：
+ *      - true 值。同时会有成功信息显示。
+ *   - 在获取失败的情况下：
+ *      - false 值。同时会有错误信息显示。
+ */
+    async getCurrentInfo(user) {
         try {
             const headers = { 'Authorization': `Token ${user}` };
             const response = await Request.get('/get-info', { headers });
@@ -74,7 +122,7 @@ const userService = {
 }
 
 export default userService;
-    // 退出时清除用户信息
+// 退出时清除用户信息
 //     const clearUserInfo = () => {
 //         userInfo.value = {};
 //         store.dispatch('doLogout');
