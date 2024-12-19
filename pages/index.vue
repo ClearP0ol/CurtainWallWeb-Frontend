fd6dccc0f0ddfff61aba62eaef1d66665a80907b
vapacity <131332667+vapacity@users.noreply.github.com>
2024-12-19 22:35:44 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,206 +1,234 @@
 <template>
-    <div class="main-container">
-      <div style="display: flex; justify-content: flex-end; margin-bottom: 5px; margin-right: 5px">
-        <el-button @click="GoToDash">进入仪表盘</el-button>
-        <el-button type="primary" @click="backToMain">返回主页</el-button>
+  <UDashboardPage>
+    <UDashboardPanel grow>
+      <UDashboardNavbar title="首页"></UDashboardNavbar>
+      <div class="main-page">
+        <UPageGrid class="custom-margin">
+          <UPageCard v-for="(module, index) in modulesLine1" :key="index" v-bind="module"
+                     @click="checkPermissionAndRedirect(module)" class="hover-effect">
+            <template #description>
+              <span class="line-clamp-2">{{ module.description }}</span>
+            </template>
+          </UPageCard>
+          <div></div>
+          <div></div>
+          <UPageCard v-for="(module, index) in modulesLine2" :key="index" v-bind="module"
+                     @click="checkPermissionAndRedirect(module)" class="hover-effect">
+            <template #description>
+              <span class="line-clamp-2">{{ module.description }}</span>
+            </template>
+          </UPageCard>
+          <UPageCard v-for="(module, index) in modulesLine3" :key="index" v-bind="module"
+                     @click="checkPermissionAndRedirect(module)" class="hover-effect">
+            <template #description>
+              <span class="line-clamp-2">{{ module.description }}</span>
+            </template>
+          </UPageCard>
+          <UPageCard v-for="(module, index) in modulesLine4" :key="index" v-bind="module"
+                     @click="checkPermissionAndRedirect(module)" class="hover-effect">
+            <template #description>
+              <span class="line-clamp-2">{{ module.description }}</span>
+            </template>
+          </UPageCard>
+        </UPageGrid>
       </div>
-      <el-divider content-position="center">
-          本地上传
-      </el-divider>
-      <div class="upload-container">
-        <!-- 显示上传后的图片 -->
-        <div v-if="imagePreviewUrl" class="uploaded-image-preview">
-          <img :src="imagePreviewUrl" alt="Uploaded Image" class="preview-img" />
-          <el-button size="small" type="danger" @click="removeImage">
-            删除图片
-            <el-icon class="el-icon--right">
-              <Close />
-            </el-icon>
-          </el-button>
-        </div>
+    </UDashboardPanel>
+  </UDashboardPage>
+</template>
 
-        <el-upload v-else
-          class="upload-demo"
-          :action="'http://localhost:5000/defect/upload'"
-          drag
-          :on-success="handleUploadSuccess"
-          :on-error="handleUploadError"
-        >
-          <el-icon class="el-icon--upload">
-            <upload-filled />
-          </el-icon>
-          <div class="el-upload__text">拖动文件至区域内或<em>点击上传</em></div>
-          <template #tip>
-            <div class="el-upload__tip">jpg/png files with a size less than 50MB</div>
-          </template>
-        </el-upload>
-      </div>
-  
-      <el-row :gutter="10">
-        <el-col :span="3">
-          <el-button type="primary" @click="startDetection">
-            开始检测
-            <el-icon class="el-icon--right">
-              <Upload />
-            </el-icon>
-          </el-button>
-        </el-col>
-      </el-row>
+<script setup>
+import axios from "axios";
+import {ref, reactive} from "vue";
+import {ElMessage} from "element-plus";
+import {useRouter} from "vue-router";
 
-      <el-divider content-position="center">
-        检测结果
-      </el-divider>
+const router = useRouter();
+const userAuth = ref({});
 
-      <el-scrollbar class="scrollbar-container">
-        <div v-if="ImgResult" class="result">
-          {{ ImgResult.value }} <!-- 显示检测结果 -->
-        </div>
-        <div v-if="processedImageUrl" class="image-preview">
-          <img :src="processedImageUrl" alt="Processed Image" class="preview-img" />
-        </div>
-      </el-scrollbar>
-      
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  import { UploadFilled } from '@element-plus/icons-vue';
-  import { Upload } from '@element-plus/icons-vue';
-  import axios from 'axios';
-  import { useRouter } from 'vue-router';
-  
-  const router = useRouter();
-  const imageUrl = ref(''); // 存储上传后的图片路径
-  const uploadedFile = ref(null); // 存储上传的文件
-  const ImgResult = ref(null); 
-  const imagePreviewUrl = ref(null); // 存储图片预览的 URL
-  const processedImageUrl = ref(null); // Store the processed image URL (newly added)
-  
-  const backToMain = () => {
-    router.push('/');
-  };
-  
-  const GoToDash = () => {
-    router.push({
-      name: 'layout',
-      params: {
-        choice: 'dashboard',
+definePageMeta({
+  middleware: "auth",
+});
+
+const getUserAuth = async () => {
+  try {
+    const authToken = localStorage.getItem("authToken");
+    const response = await axios.get("/api/account/custom/getPermissions", {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
       },
     });
-  };
-  
-  const handleUploadSuccess = (response, file, fileList) => {
-    console.log('上传成功：', response);
-    imageUrl.value = response.url; // 假设后端返回的图片 URL 在 response.url 中
-    uploadedFile.value = file.raw; // 存储上传的文件
-    imagePreviewUrl.value = URL.createObjectURL(file.raw); // 创建图片预览的 URL
-  };
-  
-  const handleUploadError = (error) => {
-    console.error('上传失败：', error);
-  };
-  
-  const handlePreview = (file) => {
-    // 处理图片预览
-    imagePreviewUrl.value = URL.createObjectURL(file.raw);
-  };
-
-  // 删除图片
-  const removeImage = () => {
-    imagePreviewUrl.value = null; // 清空图片预览
-    uploadedFile.value = null; // 清空上传的文件
-    imageUrl.value = ''; // 清空图片的 URL
-  };
-
-  const startDetection = () => {
-    if (!uploadedFile.value) {
-        console.error('请先上传图片');
-        return;
-    }
-
-    let formData = new FormData();
-    formData.append('image', uploadedFile.value);
-    console.log("发送的图片路径：", imageUrl.value);
-
-    axios
-        .post('http://localhost:5000/defect/classify', formData)
-        .then((response) => {
-        console.log('检测结果：', response.data);
-        ImgResult.value = response.data.result; // 只提取结果部分
-
-        if (ImgResult.value === 'defect') {
-            // 如果检测到 defect，调用 process_image 后端 API
-            axios
-            .post('http://localhost:5000/defect/showDefect', {
-                image_path: imageUrl.value.replace('http://localhost:5000/', ''),
-            })
-            .then((processResponse) => {
-                processedImageUrl.value = `http://localhost:5000/${processResponse.data.processed_image_path}`;
-                console.log("处理后的图片路径：", processedImageUrl.value); // 在这里记录路径，确保请求完成后
-            })
-            .catch((error) => {
-                console.error('处理图片失败：', error);
-            });
-        } else {
-            // 如果检测到 undefect，直接显示原图
-            processedImageUrl.value = imagePreviewUrl.value;
-            console.log("处理后的图片路径：", processedImageUrl.value); // 在这里记录路径，确保请求完成后
-        }
-        })
-        .catch((error) => {
-        console.error('检测失败：', error);
-        });
+    userAuth.value = response.data.data;
+    console.log(userAuth.value);
+  } catch (error) {
+    console.error("Failed to fetch permissions");
+    ElMessage.error("获取用户权限失败");
+  }
 };
+getUserAuth();
 
-  </script>
-  
-  <style scoped>
-  .main-container {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    margin: 10px;
+const modulesLine1 = reactive([
+  {
+    title: "实时数据检测",
+    description: "检测建筑风振数据，及时报告异常风振情况",
+    // to: "",
+    target_address: "/monitor",
+    permissionKey: "access_system_e",
+    icon: "i-simple-icons-tailwindcss",
   }
-  
-  .upload-container {
-    padding: 10px;
-    background: #f5f7fa;
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    text-align: center;
-    margin-bottom: 10px;
-  }
+]);
 
-  .uploaded-image-preview {
-    margin-top: 10px;
-    display: flex;
-    flex-direction: column;  
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-    justify-content: center;
-  }
+const modulesLine2 = reactive([
+  {
+    title: "3D建筑模型",
+    description: "用于查看3D建筑模型，可视化反映建筑问题",
+    // to: "http://localhost:5173",
+    target_address: "http://120.46.136.85:5173",
+    icon: "i-simple-icons-googlehome",
+    permissionKey: "access_system_a",
+  },
+  {
+    title: "石材裂缝检测",
+    description: "用于识别建筑石材幕墙表面裂缝",
+    target_address: "http://1.92.72.113:5050",
+    permissionKey: "access_system_c",
+    icon: "i-simple-icons-affinitypublisher",
+  },
+  {
+    title: "石材污渍检测",
+    description: "用于识别建筑石材幕墙表面污渍",
+    target_address: "/stonedirty/mainpage",
+    icon: "i-heroicons-fire",
+    permissionKey: "access_system_b",
+  },
+]);
 
-  .preview-img {
-    max-width: 100%;
-    height: auto;
-  }
+const modulesLine3 = reactive([
+  {
+    title: "幕墙材质分割",
+    description: "给定一张建筑幕墙图片，分割出其中的各种材质",
+    // to: "",
+    target_address: "/segment",
+    permissionKey: "access_system_f",
+    icon: "i-simple-icons-homeassistantcommunitystore",
+  },
+  {
+    title: "玻璃自爆检测",
+    description: "通过图片检测玻璃自爆风险",
+    //target_address: "/explosion",
+    to: "/spallingDetection/index.vue",
+    permissionKey: "access_system_d",
+    icon: "i-material-symbols-sound-detection-glass-break-sharp",
+  },
+  {
+    title: "玻璃平整度检测",
+    description: "给定一张建筑玻璃图片，检测其平整度",
+    to: "/smoothnessDetection/index.vue",
+    //target_address: "http://111.231.168.12:3000",
+    permissionKey: "access_system_g",
+    icon: "i-simple-icons-edgeimpulse",
+  },
+]);
 
-  .result {
-    color: brown;
-    background: #e0bebe;
-    padding: 5px;
-    width: 65px;
-    border-radius: 5px;
-    margin: 5px;
-  }
+const modulesLine4 = reactive([
+  {
+    title: "幕墙韧性评估",
+    description: "用于查看评估幕墙韧性",
+    // to: "",
+    target_address: "/segment",
+    permissionKey: "access_system_h",
+    target_address: "http://111.231.168.12:8999",
+    icon: "i-simple-icons-testcafe",
+  },
+]);
 
-  .scrollbar-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+// const modules = reactive([
+//   {
+//     title: "3D建筑模型",
+//     description: "用于查看3D建筑模型，可视化反映建筑问题",
+//     // to: "http://localhost:5173",
+//     target_address: "http://120.46.136.85:5173",
+//     icon: "i-simple-icons-googlehome",
+//     permissionKey: "access_system_a",
+//   },
+//   {
+//     title: "石材污渍检测",
+//     description: "用于识别建筑石材幕墙表面污渍",
+//     target_address: "/stonedirty/mainpage",
+//     icon: "i-heroicons-fire",
+//     permissionKey: "access_system_b",
+//   },
+//   {
+//     title: "石材裂缝检测",
+//     description: "用于识别建筑石材幕墙表面裂缝",
+//     target_address: "http://1.92.72.113:5050",
+//     permissionKey: "access_system_c",
+//     icon: "i-simple-icons-affinitypublisher",
+//   },
+//   {
+//     title: "玻璃自爆检测",
+//     description: "通过图片检测玻璃自爆风险",
+//     target_address: "/explosion",
+//     // to: "https://github.com/nuxt-community/eslint-module",
+//     permissionKey: "access_system_d",
+//     icon: "i-material-symbols-sound-detection-glass-break-sharp",
+//   },
+//   {
+//     title: "风振数据检测",
+//     description: "检测建筑风振数据，及时报告异常风振情况",
+//     // to: "",
+//     target_address: "/monitor",
+//     permissionKey: "access_system_e",
+//     icon: "i-simple-icons-tailwindcss",
+//   },
+//   {
+//     title: "幕墙材质分割",
+//     description: "给定一张建筑幕墙图片，分割出其中的各种材质",
+//     // to: "",
+//     target_address: "/segment",
+//     permissionKey: "access_system_f",
+//     icon: "i-simple-icons-homeassistantcommunitystore",
+//   },
+//   {
+//     title: "玻璃平整度检测",
+//     description: "给定一张建筑玻璃图片，检测其平整度",
+//     // to: "https://github.com/vueuse/vueuse",
+//     target_address: "http://111.231.168.12:3000",
+//     permissionKey: "access_system_g",
+//     icon: "i-simple-icons-edgeimpulse",
+//   },
+// ]);
+
+const checkPermissionAndRedirect = (module) => {
+  if (userAuth.value.is_superuser || userAuth.value[module.permissionKey]) {
+    if (module.title === "3D建筑模型" || module.title === "玻璃平整度检测" || module.title === "石材裂缝检测" || module.title === "幕墙韧性评估") {
+      window.location.href = module.target_address; // 使用window.location.href进行跳转
+    } else {
+      router.push({path: module.target_address}); // 使用router.push进行跳转
+    }
+  } else {
+    ElMessage.error("您没有权限访问此模块");
   }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.main-page {
+  overflow: auto
+}
+
+.custom-margin {
+  margin: 20px;
+}
+
+.hover-effect:hover {
+  background-color: #f0f0f0;
+  cursor: pointer;
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+.back-to-main-btn {
+  margin: 5px;
+  align-self: flex-end;
+  /* 对齐到容器的左侧 */
+}
+</style>
